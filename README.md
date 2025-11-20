4a0b519758ed74bc11c3653171219eeb9977cfce
Keith <combs.cdi@gmail.com>
2025-11-20 14:26:45 -0500

    routing update
    updated routing

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Used for a few things
 1. MultiFileUpload to upload multiple files and send it to the backemd in binary form 
 2. FolderCompare, to take two different folders compare the files to see which are not in there and then upload those to a backend, 
 3. Fakebank. A demo bank with a custom genesys chat bot
+4. Data Processor. Searchs recording transcripts by keyword or queue and creates jobs for the a backend to take up and write to file
--- client/src/pages/Default.tsx
+++ client/src/pages/Default.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 import { useRecoilValue } from 'recoil';
 import { authState } from '@/app/recoil/authState';
 import ResponsiveSidebarLayout, {NavItem} from '@/components/Nav/ResonsiveSidebarLayout';
-import { Menu, X, Home, Folder, Settings, HelpCircle, PiggyBank } from "lucide-react";
+import { Menu, X, Home, Folder, Settings, HelpCircle, PiggyBank,FileText } from "lucide-react";
 
 
 
@@ -15,6 +15,7 @@ const nav: NavItem[] = [
 { label: "FolderCompare", href: "/folder-compare", icon: <Folder className="h-4 w-4" /> },
 { label: "TTS", href: "/tts", icon: <HelpCircle className="h-4 w-4" /> },
 { label: "Bank", href: "/bank", icon: <PiggyBank className="h-4 w-4" /> },
+{ label: "Data Procesor", href: "/data", icon: <FileText className="h-4 w-4" /> },
 { label: "Settings", href: "/settings", icon: <Settings className="h-4 w-4" /> },
  ];
 
